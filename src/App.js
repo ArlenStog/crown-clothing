f0f5d5cd4da289855bd526d4434c0d2a0c1b753c
Aleksandar Mijailovic <smijailovic5@gmail.com>
2022-05-12 15:42:11 +0200

    Kreiranje custom button komponente, kao i forme za sign in

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/homepage/shop/shop.component';
 import Header from './components/header/header.component'
-
+import SignInSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
     <Switch>
     <Route exact  path='/' component={HomePage}></Route>
     <Route path='/shop' component={ShopPage}></Route>
+    <Route path='/signin' component={SignInSignOutPage}></Route>
     </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-sign-up.styles.scss'
+
+
+const SignInSignOutPage = () => (
+
+<div className='sign-in-and-sign-out'>
+<SignIn />
+</div>
+
+);
+export default SignInSignOutPage;
